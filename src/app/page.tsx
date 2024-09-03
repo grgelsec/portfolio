37ecea209d7b88d4524322f1d8dbf8053112f467
Lisec, George <glisec@iu.edu>
2024-09-03 13:31:05 -0400

    linkedIN link working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -256,7 +256,7 @@ export default async function Home() {
             />
             <SocialLink
               href="#"
-              aria-label="www.linkedin.com/in/grgelsec"
+              aria-label="https://www.linkedin.com/in/grgelsec/"
               icon={LinkedInIcon}
             />
           </div>
