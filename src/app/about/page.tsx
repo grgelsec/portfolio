e28aa741c217b3524f6512d9f5e04b16363abcb8
Lisec, George <glisec@iu.edu>
2024-09-03 13:35:50 -0400

    RAHHHHHH

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -69,8 +69,8 @@ export default function About() {
         </div>
         <div className="lg:order-first lg:row-span-2">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            I’m George Lisec. Computer Science student and obsessed with
-            learning.
+            I’m George Lisec. Computer Science student and aspiring Software
+            Engineer.
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>
