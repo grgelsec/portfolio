2387e10f306a2b7ebc50a30a774d31c13dd562ae
Lisec, George <glisec@iu.edu>
2024-09-03 10:11:14 -0400

    init commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,134 @@
+import { type Metadata } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+import clsx from 'clsx'
+
+import { Container } from '@/components/Container'
+import {
+  GitHubIcon,
+  InstagramIcon,
+  LinkedInIcon,
+  XIcon,
+} from '@/components/SocialIcons'
+import portraitImage from '@/images/portrait.jpg'
+
+function SocialLink({
+  className,
+  href,
+  children,
+  icon: Icon,
+}: {
+  className?: string
+  href: string
+  icon: React.ComponentType<{ className?: string }>
+  children: React.ReactNode
+}) {
+  return (
+    <li className={clsx(className, 'flex')}>
+      <Link
+        href={href}
+        className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+      >
+        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
+        <span className="ml-4">{children}</span>
+      </Link>
+    </li>
+  )
+}
+
+function MailIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        fillRule="evenodd"
+        d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
+      />
+    </svg>
+  )
+}
+
+export const metadata: Metadata = {
+  title: 'About',
+  description:
+    'I’m Spencer Sharp. I live in New York City, where I design the future.',
+}
+
+export default function About() {
+  return (
+    <Container className="mt-16 sm:mt-32">
+      <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
+        <div className="lg:pl-20">
+          <div className="max-w-xs px-2.5 lg:max-w-none">
+            <Image
+              src={portraitImage}
+              alt=""
+              sizes="(min-width: 1024px) 32rem, 20rem"
+              className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+            />
+          </div>
+        </div>
+        <div className="lg:order-first lg:row-span-2">
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
+            I’m George Lisec. Computer Science student and obsessed with
+            learning.
+          </h1>
+          <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+            <p>
+              Ever since I was little I was obsessed with Computers. I would
+              always find my dad tinkering with hardware and finding ways to
+              keep our family computer in tip top shape. Whether it was pirating
+              macOS or ear-bleeding windows start up sounds. I was there to ask
+              Whats that? Why do you need that? Can I try?
+            </p>
+            <p>
+              Throughout my childhood, computers became second nature so when
+              college came around I naturally decided to pursue a degree in
+              Computer Science.
+            </p>
+            <p>
+              Since then, my interest in computers became a love for programming
+              regardless if I was inside and outside of the classroom. I
+              developed a passion for building which helped me zero in on
+              Software Engineering and I become more confident in that choice
+              the more I learn. Now I am trying to gain as much experience as I
+              can whether it be through internships or side projects in order to
+              be prepared as a junior Engineer.
+            </p>
+          </div>
+        </div>
+        <div className="lg:pl-20">
+          <ul role="list">
+            <SocialLink
+              href="https://www.instagram.com/glisec/"
+              icon={InstagramIcon}
+              className="mt-4"
+            >
+              Follow on Instagram
+            </SocialLink>
+            <SocialLink
+              href="https://github.com/grgelsec"
+              icon={GitHubIcon}
+              className="mt-4"
+            >
+              Follow on GitHub
+            </SocialLink>
+            <SocialLink
+              href="www.linkedin.com/in/grgelsec"
+              icon={LinkedInIcon}
+              className="mt-4"
+            >
+              Follow on LinkedIn
+            </SocialLink>
+            <SocialLink
+              href="glisec@iu.edu"
+              icon={MailIcon}
+              className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
+            >
+              glisec@iu.edu
+            </SocialLink>
+          </ul>
+        </div>
+      </div>
+    </Container>
+  )
+}
